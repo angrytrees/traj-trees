35213728a8d055ec111dd188da0daf0446f3d650
angrytrees <vyacheslav.shiryaev@gmail.com>
2017-08-01 08:28:36 +0300

    add multiprocess

--- ForestBuilding.py
+++ ForestBuilding.py
@@ -1,6 +1,17 @@
 from forest import Forest
 from build import TreeBuilding
+import multiprocessing as mp
+import copy_reg
+import types
 import random
+
+def _pickle_method(m):
+    if m.im_self is None:
+        return getattr, (m.im_class, m.im_func.func_name)
+    else:
+        return getattr, (m.im_self, m.im_func.func_name)
+
+copy_reg.pickle(types.MethodType, _pickle_method)
 
 class ForestBuilding:
     #def __init__(self):
@@ -26,7 +37,7 @@ class ForestBuilding:
 
         return sample_trajs, sample_targets
         
-    def fit(self, trajectories, targets, n_estimators, max_radius, min_trajectories, sample_share=0.66):
+    def fit(self, trajectories, targets, n_estimators, max_radius, min_trajectories, sample_share=0.66, processes=1):
         """
         Fit random forest
         :param trajectories: list of trajectories
@@ -39,14 +50,39 @@ class ForestBuilding:
         """
         forest = Forest()
         
-        for i in range(n_estimators):
-            print 'tree number: ', i + 1
-            X_train, y_train = self.sample_traj(trajectories, targets, sample_share)
+        if processes == 1:
+            #serial option
+            trees = [self.tree_fit(trajectories, targets, max_radius, min_trajectories, sample_share) for i in range(n_estimators)]
 
-            clf = TreeBuilding()
-            tree = clf.fit(X_train, y_train, max_radius, min_trajectories)
+        else:
+            #several processess option
+            pool = mp.Pool(processes=processes)
+            results = [pool.apply_async(self.tree_fit, args=(trajectories, targets, max_radius, min_trajectories, sample_share)) for i in range(n_estimators)]
+            trees = [p.get() for p in results]
+        
+        #add trees to the forest
+        for tree in trees:
             forest.add(tree)
             
         return forest
+
+    def tree_fit(self, trajectories, targets, max_radius, min_trajectories, sample_share):
+        """
+        Get and fit single tree for the forest
+        :param forest: fores to be fit
+        :param trajectories: list of trajectories
+        :param targets: list of targets
+        :param max_radius: maximum radius in searching for decision point in trees
+        :param min_trajectories: minimum number of trajectories to split further in trees
+        :param sample_share: share of sample size
+        :return: the builded forest
+        """
+        X_train, y_train = self.sample_traj(trajectories, targets, sample_share)
+
+        clf = TreeBuilding()
+        tree = clf.fit(X_train, y_train, max_radius, min_trajectories)
+
+        return tree
+
             
         
